0df3e2d5ff0dd0f4633eddac2cbb92d237f83d94
Arcenio Souza <arcenio_neto@icloud.com>
2022-10-18 11:43:02 -0300

    feat: :sparkles: Adiciona metodos getById, put e delete

--- exercicios-springboot/src/main/java/com/curso/exerciciossb/controllers/ProdutoController.java
+++ exercicios-springboot/src/main/java/com/curso/exerciciossb/controllers/ProdutoController.java
@@ -1,10 +1,15 @@
 package com.curso.exerciciossb.controllers;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +23,14 @@ public class ProdutoController {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
-
+	
+	//Usando o mesmo método para POST e PUT
+//	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+//	public @ResponseBody Produto salvarProduto(@Valid Produto produto) {
+//		produtoRepository.save(produto);
+//		return produto;
+//	}
+	
 	@PostMapping
 	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
@@ -28,5 +40,21 @@ public class ProdutoController {
 	@GetMapping
 	public Iterable<Produto> buscarProdutos() {
 		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Produto> buscarProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+	
+	@PutMapping
+	public @ResponseBody Produto alterarProduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
 	}
 }
