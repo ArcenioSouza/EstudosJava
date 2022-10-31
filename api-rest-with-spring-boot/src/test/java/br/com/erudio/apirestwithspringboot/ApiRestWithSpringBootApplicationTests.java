62a09a4e888d5218ef360f67bd71af3e6f42b128
Arcenio Souza <arcenio_neto@icloud.com>
2022-10-31 11:51:54 -0300

    feat: :tada: Inicia novo projeto api-restfull com spring boot

---
+++ api-rest-with-spring-boot/src/test/java/br/com/erudio/apirestwithspringboot/ApiRestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.apirestwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
