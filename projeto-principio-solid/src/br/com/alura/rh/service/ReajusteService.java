47a7426e3e09f3e6615b8d7249bf4137e00a3712
Arcenio Souza <arcenio_neto@icloud.com>
2022-11-01 10:14:57 -0300

    feat: :tada: Inicia projeto principio solid

---
+++ projeto-principio-solid/src/br/com/alura/rh/service/ReajusteService.java
@@ -0,0 +1,11 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+
+public class ReajusteService {
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal valorAumento){
+
+    }
+}
